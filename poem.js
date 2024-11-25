743b5d27ebb3abc5b3f2753ca4564bd428ab7ce3
Ashish kumar laheri <ashishkumar.laheri@lpu.in>
2024-11-25 13:53:44 +0530

    p4 added

--- poem.js
+++ poem.js
@@ -6,8 +6,8 @@ angular.module('ionicApp', ['ionic'])
   $scope.items = [
     { id: 1, album: 'Time Will Show You The Path', artist: '"We all have the wrong and right but the ultimate truth is time"', image: 'img/time will.png' },
     { id: 2, album: 'Fantasy Seek Ahead', artist: '"As a child we all grew up how our world would be!"', image: 'img/fantasy.png' },
-    { id: 3, album: 'poem3', artist: 'Subtitle', image: '' },
-    { id: 4, album: 'poem4', artist: 'Subtitle', image: '' },
+    { id: 3, album: "Dreamer's Drift", artist: '"A  person who lost his way in world  of dreams, Dreams are biggest part of a life ot make a person who he is!"', image: 'img/dreamer drift.png' },
+    { id: 4, album:"Who's Real You?", artist: '"We humans have a tendency to lie and hide our self in this world of fate"', image: 'img/real you.png' },
     { id: 5, album: 'poem5', artist: 'Subtitle', image: 'img/poems.png' }
   ];
   
