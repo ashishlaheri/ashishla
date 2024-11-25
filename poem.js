fd7a9cb9965902373c0c1b530ceaa799af9d9748
Ashish kumar laheri <ashishkumar.laheri@lpu.in>
2024-11-25 15:16:47 +0530

    message

--- poem.js
+++ poem.js
@@ -8,7 +8,7 @@ angular.module('ionicApp', ['ionic'])
     { id: 2, album: 'Fantasy Seek Ahead', artist: '"As a child we all grew up how our world would be!"', image: 'img/fantasy.png' },
     { id: 3, album: "Dreamer's Drift", artist: '"A  person who lost his way in world  of dreams, Dreams are biggest part of a life ot make a person who he is!"', image: 'img/dreamer drift.png' },
     { id: 4, album:"Who's Real You?", artist: '"We humans have a tendency to lie and hide our self in this world of fate"', image: 'img/real you.png' },
-    { id: 5, album: 'poem5', artist: 'Subtitle', image: 'img/poems.png' }
+    { id: 5, album: 'KAASH', artist: 'Subtitle', image: 'img/poems.png' }
   ];
   
 });
