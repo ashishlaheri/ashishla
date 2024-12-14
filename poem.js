d6706db9d214abe5a6f7e18c6d26a89300ac98f8
Ashish kumar laheri <ashishkumar.laheri@lpu.in>
2024-12-14 14:08:39 +0530

    a new profile for my poems

--- poem.js
+++ poem.js
@@ -8,7 +8,9 @@ angular.module('ionicApp', ['ionic'])
     { id: 2, album: 'Fantasy Seek Ahead', artist: '"As a child we all grew up how our world would be!"', image: 'img/fantasy.png' },
     { id: 3, album: "Dreamer's Drift", artist: '"A  person who lost his way in world  of dreams, Dreams are biggest part of a life ot make a person who he is!"', image: 'img/dreamer drift.png' },
     { id: 4, album:"Who's Real You?", artist: '"We humans have a tendency to lie and hide our self in this world of fate"', image: 'img/real you.png' },
-    { id: 5, album: 'Reboot Night', artist: '"Some wake up from nigth where he fights in echoes in mind and then again the night started"', image: 'img/reboot night.png' }
+    { id: 5, album: 'Reboot Night', artist: '"Some wake up from nigth where he fights in echoes in mind and then again the night started"', image: 'img/reboot night.png' },
+    { id: 6, album: 'Reboot Night', artist: '"Some wake up from nigth where he fights in echoes in mind and then again the night started"', image: 'img/reboot night.png' },
+    { id: 7, album: 'Reboot Night', artist: '"Some wake up from nigth where he fights in echoes in mind and then again the night started"', image: 'img/reboot night.png' }
   ];
   
 });
